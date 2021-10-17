ebafa7e4d9181fcaaa6e2c2130322e6e600b25bf
victortanzy123 <83770436+victortanzy123@users.noreply.github.com>
2021-10-17 21:38:55 +0800

    lore carousell w/o styled button

--- src/Sections/Lore/Lore.js
+++ src/Sections/Lore/Lore.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./Lore.module.css";
 
@@ -6,11 +6,57 @@ import classes from "./Lore.module.css";
 import lore from "../../Assets/lore.png";
 
 function Lore() {
+  const [nextPage, setNextPage] = useState(0);
+
+  const togglePageHandler = () => {
+    setNextPage((prevState) => {
+      console.log("nextPage:", nextPage);
+      return prevState === 0 ? (prevState === 1 ? 2 : 1) : 2;
+    });
+  };
+
+  const previousPageHandler = () => {
+    setNextPage((prevState) => {
+      console.log("nextPage:", nextPage);
+      return prevState === 2 ? (prevState === 1 ? 0 : 1) : 0;
+    });
+  };
+  console.log(nextPage);
   return (
     <div id="lore" className={classes.loreCont}>
       <h1 className={classes.title}>LORE</h1>
 
-      <img className={classes.img} src={lore} alt="lore-background" />
+      <div className={classes.loreImgCont}>
+        <img className={classes.imgOff} src={lore} alt="lore-background" />
+        <img
+          className={`${classes.img0} ${nextPage === 0 && classes.imgOn}`}
+          src={lore}
+          alt="lore-background"
+        />
+        <img
+          className={`${classes.img} ${nextPage === 1 && classes.imgOn}`}
+          src={lore}
+          alt="lore-background"
+        />
+        <img
+          className={`${classes.img2} ${nextPage === 2 && classes.imgOn}`}
+          src={lore}
+          alt="lore-background"
+        />
+        {/* <img
+          className={`${classes.img2} ${nextPage === 2 && classes.imgOn}`}
+          src={lore}
+          alt="lore-background"
+        /> */}
+      </div>
+      <div className={classes.loreButGroup}>
+        <button onClick={previousPageHandler} type="button">
+          previous
+        </button>
+        <button onClick={togglePageHandler} type="button">
+          next
+        </button>
+      </div>
     </div>
   );
 }
