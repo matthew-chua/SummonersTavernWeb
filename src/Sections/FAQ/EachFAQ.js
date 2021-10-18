650ac2fc2a88db84afbd861b9435622bc8aa9b78
victortanzy123 <83770436+victortanzy123@users.noreply.github.com>
2021-10-18 08:46:40 +0800

    dropdown animation

--- src/Sections/FAQ/EachFAQ.js
+++ src/Sections/FAQ/EachFAQ.js
@@ -32,22 +32,26 @@ function EachFAQ({ question, answer }) {
   return (
     <div
       onClick={() => setIsOpen((prevState) => !prevState)}
-      className={isOpen ? classes.faqIndivContOpen : classes.faqIndivCont}
+      className={`${classes.faqIndivCont} ${
+        isOpen && classes.faqIndivContOpen
+      }`}
       style={
         width > 800
           ? isOpen
             ? {
                 // height: `${Math.floor(wordCounter(answer) / 15) * 3 + 12}vh`,
                 // height: "fit-content",
-                transition: "all 0.3s ease",
+                transition: "all 4s ease",
+                maxHeight: "200vh",
               }
-            : { height: "10vh" }
+            : { maxHeight: "10vh" }
           : isOpen
           ? {
               // height: `${Math.floor(wordCounter(answer) / 10) * 2 + 2}vh`,
-              height: "fit-content",
+              transition: "all 4s ease",
+              maxHeight: "200vh",
             }
-          : { height: "5vh" }
+          : { maxHeight: "5vh" }
       }
     >
       <span className={classes.text}>{question}</span>
@@ -57,24 +61,23 @@ function EachFAQ({ question, answer }) {
         size={width > 800 ? "2x" : "x"}
       />
 
-      {isOpen &&
-        answer?.map((para, index) => (
-          <span
-            style={
-              isOpen
-                ? {
-                    display: "flex",
-                    opacity: "1",
-                    transition: "all 0.3s ease",
-                    // top: `${width > 800 ? "20vh" : "15vh"}`,
-                  }
-                : { display: "none", opacity: "0" }
-            }
-            className={classes.answer}
-          >
-            {para}
-          </span>
-        ))}
+      {answer?.map((para, index) => (
+        <span
+          // style={
+          //   isOpen
+          //     ? {
+          //         display: "block",
+          //         opacity: "1",
+          //         transition: "all 0.3s ease",
+          //         // top: `${width > 800 ? "20vh" : "15vh"}`,
+          //       }
+          //     : { display: "block", opacity: "1" }
+          // }
+          className={`${classes.answer} ${isOpen && classes.open}`}
+        >
+          {para}
+        </span>
+      ))}
       {/* <hr className={classes.line} /> */}
     </div>
   );
